82fa076421d688fa301f91fc106e4ea5fadeee64
nardosteve <jackcale50@gmail.com>
2020-11-25 18:59:58 +0300

    TideEat Homescreen Design

---
+++ app/src/main/java/com/stormapps/tideeat/MainActivity.java
@@ -0,0 +1,49 @@
+package com.stormapps.tideeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button btnSignUp;
+    public Button btnSignIn;
+    public TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        txtSlogan = (TextView) findViewById(R.id.txtSlogan);
+        //Get font from the Asset folder
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/NABILA.TTF");
+
+        //Set an ActionListener to the Buttons (Signin and Signup)
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+
+    }
+}
