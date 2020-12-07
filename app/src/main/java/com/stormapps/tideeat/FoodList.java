6b752e87c0cc01cf5a4b1eb1042e42eec37d208d
nardosteve <jackcale50@gmail.com>
2020-12-07 11:00:12 +0300

    Food Category List: On-going Development

---
+++ app/src/main/java/com/stormapps/tideeat/FoodList.java
@@ -0,0 +1,33 @@
+package com.stormapps.tideeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FoodList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference foodList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_list);
+
+        database = FirebaseDatabase.getInstance();
+        foodList = database.getReference("Foods");
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_food);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+}
